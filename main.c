89ced2e452c7857648aff590ea192d6b5958a12c
NarainMassand <massandnarain@gmail.com>
2020-03-17 16:25:53 -0700

    Fix the outer loop

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ db_t *head = NULL, *target = NULL;
 
 int main(int argc, char* argv[])
 {   
-    char exec[] = "dbcli", input_string[100];
+    char exec[] = "dbcli", input_string[100], newString[20][20];
     db_t *new_node, *current;
     uint64_t id = 0;
-    int result_open, result_action, choice = -1, req_args = 4;
+    int result_open, result_action, choice = -1, req_args = 4, j_iter = 0, ctr = 0;
     // malloc head
     printf("MS1\n");
     //head = malloc(sizeof(db_t));
@@ -21,6 +21,10 @@ int main(int argc, char* argv[])
     // checking for input correctness
     do
     {   
+        id = 0;
+        choice = -1;
+        j_iter = 0;
+        ctr = 0;
         
         if(argc != req_args || strcmp(exec,argv[0]) != 0)
         {
@@ -109,26 +113,35 @@ int main(int argc, char* argv[])
         
         printf("\nEnter 1 to continue or 0 to exit\n");
         scanf("%d", &choice);
+        getchar();  // to capture the additional new line character left in by scanf
         if (choice == 0)
         {
             return 0;
         }    
+
         printf("Enter next command in the form dbcli <filename> <action> <id> : ");
-        scanf("%s", input_string);
-        //fgets(input_string, sizeof(input_string), stdin);
-        char * token = strtok(input_string, " ");
-        int index = 0;
-        //argv[index] = token;
-        while( token != NULL ) 
-        {   
-            argv[index] = token;
-            index+=1;
-            token = strtok(NULL, " ");
+        fgets(input_string, sizeof input_string, stdin);
+
+        for(int idx=0; idx <= (strlen(input_string)); idx++)
+        {
+        // if space or NULL found, assign NULL into newString[ctr]
+            if(input_string[idx]==' '||input_string[idx]=='\0')
+            {
+                newString[ctr][j_iter]='\0';
+                ctr++;  //for next word
+                j_iter=0;    //for next word, init index to 0
+            }
+            else
+            {
+                newString[ctr][j_iter]=input_string[idx];
+                j_iter++;
+            }
         }
-        argc = index;
-        printf("%s %s %s %s\n", argv[0], argv[1], argv[2], argv[3]);
-        // for(int index=0 ; index < req_args ; index++)
-        //    scanf("%s", &argv[index]);
+        for(int idx=0; idx < ctr; idx++)
+            strcpy(argv[idx], newString[idx]);
+        
+        argc = ctr;  //counting the number of terms
+        //printf("%s %s %s %s %d\n", argv[0], argv[1], argv[2], argv[3], argc);
 
     } while (choice != 0 );
 }
